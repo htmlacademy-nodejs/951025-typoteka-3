698a78caec0cce3e8eeaa7c02c554a683f05a315
Andrew <andrewdruzhaev@gmail.com>
2021-03-16 15:25:39 +0300

    Created cli programm to generate mocks

---
+++ src/utils.js
@@ -0,0 +1,19 @@
+module.exports.getRandomInt = (min, max) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+module.exports.shuffle = (arr) => {
+  for (let i = arr.length - 1; i > 0; i--) {
+    const randomPosition = Math.floor(Math.random() * i);
+    [arr[i], arr[randomPosition]] = [arr[randomPosition], arr[i]];
+  }
+
+  return arr;
+};
+
+module.exports.getRandomDate = (value) => {
+  return new Date(Date.now() - value);
+};

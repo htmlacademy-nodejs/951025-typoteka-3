4ab28b429ea6b25d6010cafe364b58781ab3741f
Andrew <andrewdruzhaev@gmail.com>
2021-05-08 21:32:06 +0300

    Added templates for pages with pug

--- src/express/index.js
+++ src/express/index.js
@@ -1,13 +1,20 @@
 const express = require(`express`);
+const path = require(`path`);
+
+const DEFAULT_PORT = 8080;
+const PUBLIC_DIR = `public`;
+const TEMPLATES_DIR = `templates`;
 
 const app = express();
-const DEFAULT_PORT = 8080;
+app.set(`views`, path.resolve(__dirname, TEMPLATES_DIR));
+app.set(`view engine`, `pug`);
 
 const mainRoutes = require(`./routes/main-routes`);
 const myRoutes = require(`./routes/my-routes`);
 const arcticlesRoutes = require(`./routes/articles-routes`);
 const utilsRoutes = require(`./routes/utils-routes`);
 
+app.use(express.static(path.resolve(__dirname, PUBLIC_DIR)));
 app.use(`/`, mainRoutes);
 app.use(`/my`, myRoutes);
 app.use(`/articles`, arcticlesRoutes);
--- src/express/routes/articles-routes.js
+++ src/express/routes/articles-routes.js
@@ -1,9 +1,9 @@
 const {Router} = require(`express`);
 const arcticlesRoutes = new Router();
 
-arcticlesRoutes.get(`/category/:id`, (req, res) => res.send(`/articles/category/:id`));
-arcticlesRoutes.get(`/add`, (req, res) => res.send(`/articles/add`));
+arcticlesRoutes.get(`/category/:id`, (req, res) => res.render(`articles-by-category`));
+arcticlesRoutes.get(`/add`, (req, res) => res.render(`new-post`));
 arcticlesRoutes.get(`/edit/:id`, (req, res) => res.send(`/articles/edit/:id`));
-arcticlesRoutes.get(`/:id`, (req, res) => res.send(`/articles/:id`));
+arcticlesRoutes.get(`/:id`, (req, res) => res.render(`post`));
 
 module.exports = arcticlesRoutes;

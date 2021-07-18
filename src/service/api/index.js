7659d9ae189d4ad9c1d5c4035ed495a0cd5f0786
Andrew <andrewdruzhaev@gmail.com>
2021-07-18 20:12:18 +0300

    Create entry points for data-service & api

---
+++ src/service/api/index.js
@@ -0,0 +1,24 @@
+const {
+  ArticleService,
+  CategoryService,
+  CommentService,
+  SearchService,
+} = require(`../data-service`);
+
+const {Router} = require(`express`);
+const article = require(`./article`);
+const category = require(`./category`);
+const search = require(`./search`);
+const getMockData = require(`../lib/get-mock-data`);
+
+const app = new Router();
+
+(async () => {
+  const mockData = getMockData();
+
+  article(app, new ArticleService(mockData), new CommentService());
+  category(app, new CategoryService(mockData));
+  search(app, new SearchService(mockData));
+})();
+
+module.exports = app;

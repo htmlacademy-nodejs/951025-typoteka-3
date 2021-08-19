e2a875654e1a586793340202b855b8fb3f60ffa2
Andrew <andrewdruzhaev@gmail.com>
2021-08-19 09:50:35 +0300

    Refactor CommentService

--- src/service/data-service/comment.js
+++ src/service/data-service/comment.js
@@ -2,31 +2,31 @@ const {nanoid} = require(`nanoid`);
 const {MAX_ID_LENGTH} = require(`../../const`);
 
 class CommentService {
-  create(offer, comment) {
+  create(article, comment) {
     const newComment = Object.assign({
       id: nanoid(MAX_ID_LENGTH),
     }, comment);
 
-    offer.comments.push(newComment);
+    article.comments.push(newComment);
     return newComment;
   }
 
-  delete(offer, commentId) {
-    const deletedComment = offer.comments
+  delete(article, commentId) {
+    const deletedComment = article.comments
       .find((comment) => comment.id === commentId);
 
     if (!deletedComment) {
       return null;
     }
 
-    offer.comments = offer.comments
+    article.comments = article.comments
       .filter((comment) => comment.id !== commentId);
 
     return deletedComment;
   }
 
-  getAll(offer) {
-    return offer.comments;
+  getAll(article) {
+    return article.comments;
   }
 }
 

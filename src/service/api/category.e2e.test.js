a7c8daedaaf560961af833bf8a7e2bd59a2ac107
Andrew <andrewdruzhaev@gmail.com>
2021-08-21 01:36:25 +0300

    Refactor category tests

--- src/service/api/category.e2e.test.js
+++ src/service/api/category.e2e.test.js
@@ -1,25 +1,30 @@
-const express = require(`express`);
 const request = require(`supertest`);
 const category = require(`./category`);
 const CategoryService = require(`../data-service/category`);
+const createApi = require(`../helpers/create-api`);
 const {HttpCode} = require(`../../const`);
 const mockData = require(`../../data/mocks-data-category.json`);
 
-const app = express();
-app.use(express.json());
-category(app, new CategoryService(mockData));
+let app = null;
+let response = null;
+let categoryService = null;
 
 describe(`Api returns category list`, () => {
-  let response;
-
-  beforeAll(async () => {
-    response = await request(app)
-      .get(`/categories`);
+  beforeEach(async () => {
+    categoryService = new CategoryService(mockData);
+    app = createApi(category, [categoryService]);
   });
 
-  test(`Returns status code 200`, () => expect(response.statusCode).toBe(HttpCode.OK));
-  test(`Returns 9 categories`, () => expect(response.body.length).toBe(9));
-  test(`Category names are 'Разное', 'Кино', 'Музыка', 'Деревья', 'IT', 'Без рамки', 'Программирование', 'За жизнь', 'Желез'`, () => {
+  test(`Returns status code 200`, async () => {
+    response = await request(app).get(`/categories`);
+    expect(response.statusCode).toBe(HttpCode.OK);
+  });
+  test(`Returns 9 categories`, async () => {
+    response = await request(app).get(`/categories`);
+    expect(response.body.length).toBe(9);
+  });
+  test(`Category names are 'Разное', 'Кино', 'Музыка', 'Деревья', 'IT', 'Без рамки', 'Программирование', 'За жизнь', 'Желез'`, async () => {
+    response = await request(app).get(`/categories`);
     expect(response.body).toEqual(expect.arrayContaining([`Разное`, `Кино`, `Музыка`, `Деревья`, `IT`, `Без рамки`, `Программирование`, `За жизнь`, `Желез`]));
   });
 });

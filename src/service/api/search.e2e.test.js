c4dcdae160f6be1af8ed1ea875c37ce7fe36c9a2
Andrew <andrewdruzhaev@gmail.com>
2021-08-21 01:29:56 +0300

    Refactor search tests

--- src/service/api/search.e2e.test.js
+++ src/service/api/search.e2e.test.js
@@ -5,37 +5,60 @@ const SearchService = require(`../data-service/search`);
 const {HttpCode} = require(`../../const`);
 const mockData = require(`../../data/mock-data-search.json`);
 
-const app = express();
-app.use(express.json());
-search(app, new SearchService(mockData));
+const createApi = (services) => {
+  const app = express();
+  app.use(express.json());
+  search(app, ...services);
+  return app;
+};
 
-describe(`API returns article based on search query`, () => {
-  let response;
+let app = null;
+let response = null;
+let searchService = null;
 
-  beforeAll(async () => {
-    response = await request(app)
-      .get(`/search`)
-      .query({
+describe(`Search`, () => {
+  describe(`API returns article based on query`, () => {
+    beforeEach(async () => {
+      searchService = new SearchService(mockData);
+      app = createApi([searchService]);
+    });
+    test(`Status code 200`, async () => {
+      response = await request(app).get(`/search`).query({
         query: `Учим HTML и CSS`,
       });
-  });
-  test(`Returns status code 200`, () => expect(response.statusCode).toBe(HttpCode.OK));
-  test(`Returns only one article`, () => expect(response.body.length).toBe(1));
-  test(`The article id is correct`, () => expect(response.body[0].id).toBe(`fHqesx_R`));
-});
 
-describe(`Negative API scenarios`, () => {
-  test(`Status code 404 if article is not found`, (done) => {
-    request(app)
-      .get(`/search`)
-      .query({
+      expect(response.statusCode).toBe(HttpCode.OK);
+    });
+    test(`Returns one article`, async () => {
+      response = await request(app).get(`/search`).query({
+        query: `Учим HTML и CSS`,
+      });
+
+      expect(response.body.length).toBe(1);
+    });
+    test(`The article id is correct`, async () => {
+      response = await request(app).get(`/search`).query({
+        query: `Учим HTML и CSS`,
+      });
+
+      expect(response.body[0].id).toBe(`fHqesx_R`);
+    });
+  });
+
+  describe(`API refuses to return offer`, () => {
+    beforeEach(async () => {
+      searchService = new SearchService(mockData);
+      app = createApi([searchService]);
+    });
+    test(`Status code 404 if article is not found`, (done) => {
+      request(app).get(`/search`).query({
         query: `No article`,
-      })
-      .expect(HttpCode.NOT_FOUND, done);
+      }).expect(HttpCode.NOT_FOUND, done);
+    });
+    test(`Status code 400 if query string is absent`, (done) => {
+      request(app).get(`/search`)
+        .expect(HttpCode.BAD_REQUEST, done);
+    });
   });
-  test(`Status code 400 if query string is absent`, (done) => {
-    request(app)
-      .get(`/search`)
-      .expect(HttpCode.BAD_REQUEST, done);
-  });
+
 });

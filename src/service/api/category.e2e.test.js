992553ccb5aedc5fd4aa1c51c132aff318e3b5dd
Andrew <andrewdruzhaev@gmail.com>
2021-08-21 01:37:53 +0300

    Update category tests

--- src/service/api/category.e2e.test.js
+++ src/service/api/category.e2e.test.js
@@ -9,22 +9,24 @@ let app = null;
 let response = null;
 let categoryService = null;
 
-describe(`Api returns category list`, () => {
-  beforeEach(async () => {
-    categoryService = new CategoryService(mockData);
-    app = createApi(category, [categoryService]);
-  });
+describe(`category`, () => {
+  describe(`Api returns category list`, () => {
+    beforeEach(async () => {
+      categoryService = new CategoryService(mockData);
+      app = createApi(category, [categoryService]);
+    });
 
-  test(`Returns status code 200`, async () => {
-    response = await request(app).get(`/categories`);
-    expect(response.statusCode).toBe(HttpCode.OK);
-  });
-  test(`Returns 9 categories`, async () => {
-    response = await request(app).get(`/categories`);
-    expect(response.body.length).toBe(9);
-  });
-  test(`Category names are 'Разное', 'Кино', 'Музыка', 'Деревья', 'IT', 'Без рамки', 'Программирование', 'За жизнь', 'Желез'`, async () => {
-    response = await request(app).get(`/categories`);
-    expect(response.body).toEqual(expect.arrayContaining([`Разное`, `Кино`, `Музыка`, `Деревья`, `IT`, `Без рамки`, `Программирование`, `За жизнь`, `Желез`]));
+    test(`Status code 200`, async () => {
+      response = await request(app).get(`/categories`);
+      expect(response.statusCode).toBe(HttpCode.OK);
+    });
+    test(`Returns 9 categories`, async () => {
+      response = await request(app).get(`/categories`);
+      expect(response.body.length).toBe(9);
+    });
+    test(`Category names are 'Разное', 'Кино', 'Музыка', 'Деревья', 'IT', 'Без рамки', 'Программирование', 'За жизнь', 'Желез'`, async () => {
+      response = await request(app).get(`/categories`);
+      expect(response.body).toEqual(expect.arrayContaining([`Разное`, `Кино`, `Музыка`, `Деревья`, `IT`, `Без рамки`, `Программирование`, `За жизнь`, `Желез`]));
+    });
   });
 });

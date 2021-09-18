ed70b28e4d34f8c8cb3e12f7f0e7b7937967cefd
Andrew <andrewdruzhaev@gmail.com>
2021-09-18 18:35:27 +0300

    Install dayjs & update main route

--- src/express/routes/main-routes.js
+++ src/express/routes/main-routes.js
@@ -1,11 +1,17 @@
 const {Router} = require(`express`);
 const api = require(`../api`).getAPI();
+const dayjs = require(`dayjs`);
 const mainRoutes = new Router();
 
 mainRoutes.get(`/`, async (req, res) => {
   try {
     const articles = await api.getArticles();
-    res.render(`main`, {articles});
+    res.render(`main`, {articles: articles.map((article) => {
+      article.date = dayjs(article.createdDate).format(`YYYY-MM-DDTHH:mm`);
+      article.outputDate = dayjs(article.createdDate).format(`DD.MM.YYYY, HH:mm`);
+
+      return article;
+    })});
   } catch (error) {
     console.log(error);
   }

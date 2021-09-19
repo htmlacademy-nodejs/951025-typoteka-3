fc088398405a02038b89589284533333c1bf0561
Andrew <andrewdruzhaev@gmail.com>
2021-09-19 13:08:21 +0300

    Get data for post & update template

--- src/express/routes/articles-routes.js
+++ src/express/routes/articles-routes.js
@@ -1,9 +1,20 @@
 const {Router} = require(`express`);
+const api = require(`../api`).getAPI();
+const {modifiedArticle} = require(`../helpers/articles`);
 const arcticlesRoutes = new Router();
+
+const currentPost = async (req, res) => {
+  try {
+    const article = await api.getArticle(req.params.id);
+    res.render(`post`, {article: modifiedArticle(article)});
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 arcticlesRoutes.get(`/category/:id`, (req, res) => res.render(`articles-by-category`));
 arcticlesRoutes.get(`/add`, (req, res) => res.render(`new-post`));
-arcticlesRoutes.get(`/edit/:id`, (req, res) => res.send(`/articles/edit/:id`));
-arcticlesRoutes.get(`/:id`, (req, res) => res.render(`post`));
+arcticlesRoutes.get(`/edit/:id`, currentPost);
+arcticlesRoutes.get(`/:id`, currentPost);
 
 module.exports = arcticlesRoutes;

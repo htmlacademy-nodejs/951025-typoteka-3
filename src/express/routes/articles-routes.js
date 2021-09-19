8eac00e858c877a5cf5a0f0324f9f3a30dc5f7bd
Andrew <andrewdruzhaev@gmail.com>
2021-09-19 15:57:33 +0300

    Configure multer.diskStorage & update template

--- src/express/routes/articles-routes.js
+++ src/express/routes/articles-routes.js
@@ -1,7 +1,25 @@
 const {Router} = require(`express`);
 const api = require(`../api`).getAPI();
 const {modifiedArticle} = require(`../helpers/articles`);
+const multer = require(`multer`);
+const path = require(`path`);
+const {nanoid} = require(`nanoid`);
+
+const UPLOAD_DIR = `../upload/img`;
+const uploadDirAbsolute = path.resolve(__dirname, UPLOAD_DIR);
 const arcticlesRoutes = new Router();
+
+const storage = multer.diskStorage({
+  destination: uploadDirAbsolute,
+
+  filename: (req, file, cb) => {
+    const uniqueName = nanoid(10);
+    const extension = file.originalname.split(`.`).pop();
+    cb(null, `${uniqueName}.${extension}`);
+  }
+});
+
+const upload = multer({storage});
 
 const currentPost = async (req, res) => {
   try {
@@ -13,7 +31,12 @@ const currentPost = async (req, res) => {
 };
 
 arcticlesRoutes.get(`/category/:id`, (req, res) => res.render(`articles-by-category`));
-arcticlesRoutes.get(`/add`, (req, res) => res.render(`new-post`));
+
+arcticlesRoutes.get(`/add`, async (req, res) => {
+  const categories = await api.getCategories();
+  res.render(`new-post`, {categories});
+});
+
 arcticlesRoutes.get(`/edit/:id`, currentPost);
 arcticlesRoutes.get(`/:id`, currentPost);
 

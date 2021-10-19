dc8a45292f6f42aa4e314cbb5db437384ae76a80
Andrew <andrewdruzhaev@gmail.com>
2021-10-19 10:10:26 +0300

    Create schema with tables

---
+++ schema.sql
@@ -0,0 +1,42 @@
+CREATE TABLE categories(
+  id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  name VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE users(
+  id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  email VARCHAR(255) UNIQUE NOT NULL,
+  first_name VARCHAR(255) NOT NULL,
+  last_name VARCHAR(255) NOT NULL,
+  password_hash VARCHAR(50) NOT NULL,
+  avatar VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE articles(
+  id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  title VARCHAR(255) NOT NULL,
+  photo VARCHAR(50),
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  announcement VARCHAR(255) NOT NULL,
+  full_text text,
+  user_id INTEGER NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+CREATE TABLE comments(
+  id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  user_id INTEGER NOT NULL,
+  article_id INTEGER NOT NULL,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  text text NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES users(id),
+  FOREIGN KEY (article_id) REFERENCES articles(id)
+);
+
+CREATE TABLE article_categories(
+  article_id INTEGER NOT NULL,
+  category_id INTEGER NOT NULL,
+  PRIMARY KEY (article_id, category_id),
+  FOREIGN KEY (article_id) REFERENCES articles(id),
+  FOREIGN KEY (category_id) REFERENCES categories(id)
+);

c6a685ab4d91362923b702e26cece8bef5a3e60d
Andrew <andrewdruzhaev@gmail.com>
2021-11-07 16:15:37 +0300

    Update models relationships

--- src/service/models/index.js
+++ src/service/models/index.js
@@ -2,23 +2,31 @@ const Aliase = require(`./aliase`);
 const defineCategory = require(`./category`);
 const defineComment = require(`./comment`);
 const defineArticle = require(`./article`);
+const defineUser = require(`./user`);
 const {Model} = require(`sequelize`);
-
-class ArticleCategory extends Model {}
 
 module.exports = (sequelize) => {
   const Category = defineCategory(sequelize);
   const Comment = defineComment(sequelize);
   const Article = defineArticle(sequelize);
+  const User = defineUser(sequelize);
 
   Article.hasMany(Comment, {as: Aliase.COMMENTS, foreignKey: `articleId`, onDelete: `cascade`});
   Comment.belongsTo(Article, {foreignKey: `articleId`});
 
-  ArticleCategory.init({}, {sequelize});
+  User.hasMany(Article, {as: Aliase.ARTICLES, foreignKey: `userId`, onDelete: `cascade`});
+  Article.belongsTo(User, {foreignKey: `userId`});
 
-  Article.belongsToMany(Category, {through: ArticleCategory, as: Aliase.CATEGORIES});
-  Category.belongsToMany(Article, {through: ArticleCategory, as: Aliase.ARTICLES});
-  Category.hasMany(ArticleCategory, {as: Aliase.ARTICLE_CATEGORIES});
+  User.hasMany(Comment, {as: Aliase.COMMENTS, foreignKey: `userId`, onDelete: `cascade`});
+  Comment.belongsTo(User, {foreignKey: `userId`});
 
-  return {ArticleCategory, Article, Category, Comment};
+  class ArticleCategories extends Model {}
+  ArticleCategories.init({}, {sequelize});
+
+  Article.belongsToMany(Category, {through: ArticleCategories, as: Aliase.CATEGORIES});
+  Category.belongsToMany(Article, {through: ArticleCategories, as: Aliase.ARTICLES});
+  Category.hasMany(ArticleCategories, {as: Aliase.ARTICLE_CATEGORIES});
+  Article.hasMany(ArticleCategories, {as: Aliase.ARTICLE_CATEGORIES});
+
+  return {ArticleCategories, Article, Category, Comment, User};
 };
